84801ac360fb43143429be9e8edad3f7a3c34321
mai cook <mailiecook@yahoo.com>
2020-05-19 10:55:30 -0700

    finished functionality

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,28 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
+    public class HomeController : Controller
+    {
 
-    [HttpGet("/")]
-    public ActionResult Index()
-    {
-      Item starterItem = new Item("Add first item To Do List");
-      return View(starterItem);
-    }
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return View("Index", myItem);
-    }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
 
-  }
+    }
 }
